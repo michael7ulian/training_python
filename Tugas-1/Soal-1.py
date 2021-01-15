07c6cf1dac4a1439e988d2db3cb5dab9a7661c74
Michael Julian <michael.julian@cp.co.id>
2021-01-15 14:29:19 +0700

    update Tugas-1

---
+++ Tugas-1/Soal-1.py
@@ -0,0 +1,4 @@
+Nama = input("siapa nama anda? ") 
+Umur = int(input("berapa umur anda? "))
+Tinggi = float(input("berapa tinggi anda? "))
+print(f"Nama saya {Nama}, umur saya {Umur} tahun dan tinggi saya {Tinggi} cm.")
---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,4 @@
+pi = 22/7
+r = int(input("masukan jari-jari lingkaran: "))
+luas_lingkaran = pi*r*r
+print(f"Luas lingkaran dengan jari-jari {r} cm adalah {'{0:.2f}'.format(luas_lingkaran)} cm\u00b2")
